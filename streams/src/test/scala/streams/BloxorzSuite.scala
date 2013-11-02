14833f354c393fdf6914b5e0b9730f7fc28257e4
kyohiro <kyohiro@gmail.com>
2013-11-02 20:51:42 +0800

    Assignment 6 - Streams

---
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -0,0 +1,118 @@
+package streams
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import Bloxorz._
+
+@RunWith(classOf[JUnitRunner])
+class BloxorzSuite extends FunSuite {
+
+  trait SolutionChecker extends GameDef with Solver with StringParserTerrain {
+    /**
+     * This method applies a list of moves `ls` to the block at position
+     * `startPos`. This can be used to verify if a certain list of moves
+     * is a valid solution, i.e. leads to the goal.
+     */
+    def solve(ls: List[Move]): Block =
+      ls.foldLeft(startBlock) { case (block, move) => move match {
+        case Left => block.left
+        case Right => block.right
+        case Up => block.up
+        case Down => block.down
+      }
+    }
+  }
+
+  trait Level1 extends SolutionChecker {
+      /* terrain for level 1*/
+
+    val level =
+    """ooo-------
+      |oSoooo----
+      |ooooooooo-
+      |-ooooooooo
+      |-----ooToo
+      |------ooo-""".stripMargin
+      
+     
+
+    val optsolution = List(Right, Right, Down, Right, Right, Right, Down)
+  }
+  
+  trait Level3 extends Level1 {
+    override val level =
+      """------ooooooo--
+        |oooo--ooo--oo--
+        |ooooooooo--oooo
+        |oSoo-------ooTo
+        |oooo-------oooo
+        |------------ooo""".stripMargin
+  }
+  
+  
+  test("find neighbours") {
+    new Level1 {
+      val s1 = neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet
+      val s2 = Set(
+              (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+              (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+              )
+      assert(s1 === s2, true)
+    }
+  }
+    
+  test("avoid circles") {
+      new Level1 {
+        val s1 = newNeighborsOnly(
+          Set(
+            (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+            (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+          ).toStream,
+        
+          Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+        ) 
+        val s2 = Set((Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))).toStream
+        assert(s1 === s2, true)
+      }
+  }
+
+  test("terrain function level 1") {
+    new Level1 {
+      assert(terrain(Pos(0,0)), "0,0")
+      assert(!terrain(Pos(4,11)), "4,11")
+    }
+  }
+
+  test("findChar level 1") {
+    new Level1 {
+      assert(startPos == Pos(1,1))
+    }
+  }
+
+  test("optimal solution for level 1") {
+    new Level1 {
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+  
+  test("opsolution works") {
+    new Level1 {
+      assert(solve(optsolution) == Block(goal, goal))
+    }
+  }
+
+  test("optimal solution length for level 1") {
+    new Level1 {
+      val s1 = solution
+      val s2 = optsolution
+      assert(solution.length == optsolution.length)
+    }
+  }
+  
+  test("level 3") {
+    new Level3 {
+      assert(solution.length === 19)
+    }
+  }
+}
